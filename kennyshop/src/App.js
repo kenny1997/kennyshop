26e562a8444e80123dd1f5655e49b4a5c061c13a
kenny1997 <1562266298@qq.com>
2021-10-26 15:43:53 +0800

    product.js

--- kennyshop/src/App.js
+++ kennyshop/src/App.js
@@ -3,14 +3,14 @@ import './App.css';
 import { Container } from 'react-bootstrap'
 import Footer from './components/Footer';
 import Header from './components/Header';
-
+import HomeScreen from './screens/HomeScreen';
 function App() {
   return (
     <>
       <Header />
       <main className='py-3'>
         <Container>
-          <h1>kennyshop</h1>
+          <HomeScreen />
         </Container>
 
       </main>
---
+++ kennyshop/src/components/Product.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Card } from 'react-bootstrap'
+
+const Product = (props) => {
+    const { product } = props
+    return (
+        <Card className='my-3 p-3 rounded'>
+            <a href={`/product/${product._id}`}></a>
+            <Card.Img src={product.image} variant='top' />
+        </Card>
+    )
+}
+
+export default Product
